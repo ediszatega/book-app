fecf1b884567c3e2a81c238a191cd1e79a83b62e
Edis Zatega <ediszatega@hotmail.com>
2024-02-09 18:25:46 +0100

    project setup

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "background-color": "#f7f8f5",
+        "primary-color": "#e85527",
+        "secondary-color": "#19394c",
+      },
+    },
+    container: {
+      padding: "2rem",
+      center: true,
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+    },
+  },
+  plugins: [],
+};
